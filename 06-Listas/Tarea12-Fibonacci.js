7e63252d37de2a857ddb71f3e2ff00270b9b54d5
Alejandro Fernandez <srvf2019@gmail.com>
2022-11-16 21:32:57 +0100

    12

---
+++ 06-Listas/Tarea12-Fibonacci.js
@@ -0,0 +1,15 @@
+
+
+function Fibonnaci(num){
+    let i = 0
+    let Resultado = [1]
+    let numero = 1
+    while ( i < (num-1)){
+        Resultado.push(numero)
+        numero = numero + Resultado[i]
+        i++
+    }
+    return Resultado
+}
+
+Fibonnaci(6)
