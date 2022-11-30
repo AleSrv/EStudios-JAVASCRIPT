681ba61625bea08b89bc012a17a45edbd23c4f9d
Alejandro Fernandez <srvf2019@gmail.com>
2022-11-30 18:22:39 +0100

    15

--- 15-Persistencia-de-Datos/TP15/index.js
+++ 15-Persistencia-de-Datos/TP15/index.js
@@ -3,11 +3,11 @@ const ape = "Fernandez"
 
 const yo = {nombre: nom, apellido:ape}
 
-sessionStorage.setItem("temporal",JSON.stringify(yo))
-console.log(JSON.parse(sessionStorage.getItem("temporal")))
+// sessionStorage.setItem("temporal",JSON.stringify(yo))
+// console.log(JSON.parse(sessionStorage.getItem("temporal")))
 
-localStorage.setItem("local",JSON.stringify(yo))
-console.log(JSON.parse(localStorage.getItem("local")))
+// localStorage.setItem("local",JSON.stringify(yo))
+// console.log(JSON.parse(localStorage.getItem("local")))
 
-document.cookie = `2minutos = ${JSON.stringify(yo)};max-age=120`
-console.log(document.cookie)
+// document.cookie = `2minutos = ${JSON.stringify(yo)};max-age=120`
+// console.log(document.cookie)
